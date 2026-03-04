c4c402b0236e4d4f221b6ae54aafe2a9af6eb776
Cương Nguyễn <cuong0993@gmail.com>
2026-03-04 11:14:08 +0700

    Do not use trivy (yet)

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -4,8 +4,8 @@ return {
     event = { "BufWritePost", "BufReadPost" },
     config = function()
       require("lint").linters_by_ft = {
-        terraform = { "terraform_validate", "trivy", "tflint" },
-        tf = { "terraform_validate", "trivy", "tflint" },
+        terraform = { "terraform_validate", "tflint" },
+        tf = { "terraform_validate", "tflint" },
         ["terraform-vars"] = { "terraform_validate", "tfsec", "tflint" },
         dockerfile = { "hadolint" },
         go = { "golangcilint" },
